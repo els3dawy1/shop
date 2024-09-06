e4789a90eb52ed9f075f5801b8b23515e37be614
els3dawy1 <168979204+els3dawy1@users.noreply.github.com>
2024-09-06 22:00:59 +0300

    Add files via upload

---
+++ project/rakwan-shop/admin/config.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect('localhost','els3dawy','123456','shop_db');
+?>
---
+++ project/rakwan-shop/config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('localhost','els3dawy','123456','shop_db') or die('connection failed');
+
+?>
